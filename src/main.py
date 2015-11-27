b1f688a7d0d43feb693c4e302fb878a61a0f1a64
Gugas <gugaswhom@gmail.com>
2015-11-27 12:44:13 +0000

    Added the csv file handling to files.py

--- src/main.py
+++ src/main.py
@@ -5,4 +5,4 @@ def main():
 	pass
 
 if __name__ == '__main__':
-	main()
+		main()
--- src/utils/files.py
+++ src/utils/files.py
@@ -1,6 +1,8 @@
 from os import listdir
 from os.path import isfile, join, isdir
 
+import csv
+
 def listDir(dir_path):
     if not isdir(dir_path):
         return None
@@ -11,3 +13,18 @@ def searchDir(dir_path):
         return None
     return [with join(dir_path, f) as p
             for f in listdir(dir_path) if isfile(p))]
+
+def readCSV(filename, delimit=','):
+	with open(filename) as csvfile:
+		data = csv.reader(csvfile, delimiter=delimit)
+	return data
+
+def writeCSV(filename, data, delimit=',', mode='w'):
+	#NTS: document exactly how the data structure should be
+	with open(filename, mode) as csvfile:
+		writer = csv.writer(csvfile, delimiter=delimit)
+		nlines=0
+		for element in data:
+			writer.writerow(element)
+			nlines += 1
+	return nlines #to return the number of lines writen
