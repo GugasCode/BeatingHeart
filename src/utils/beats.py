2785d250710b07db9d4dc527c6b71ac259912be6
Gustavo <gugaswhom@gmail.com>
2015-11-30 10:59:05 +0000

    Coded the average_noise_reduction in beats.py

--- src/utils/beats.py
+++ src/utils/beats.py
@@ -7,3 +7,13 @@ import numpy as np
 
 def fourier(data):
     return np.fft(data)
+
+def averageReduction(data):
+    """
+    Function that will perform the average method for nosie reduction. First it
+    calculates the average value of the data and then cuts everything that is
+    within that interval in order to reduce the amount of noise to heartbeat
+    ratio.
+    """
+    mean = np.average(data)
+    return data
