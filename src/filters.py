ff0dc443f4d7b504d97e3e4c53ee0fc44ea7793d
DrHell <jose.filipe.m@gmail.com>
2015-12-02 14:24:09 +0000

    Merged filter files

--- src/filters.py
+++ src/filters.py
@@ -1,6 +1,20 @@
 import numpy as np
 from sklearn.preprocessing import normalize
 
+def convertFR(frames, fr=2000):
+    """
+    Function that will convert the frame rate of a given file to a smaller one.
+    """
+    total_frames = len(frames)
+    ratio = total_frames % fr
+    new_frames = []
+    count = 0
+    for i in frames[::ratio]:
+        # we'll select the maximum value for the ratio interval
+        new_frames.append(max(frames[count*ratio:count*ratio+ratio]))
+        # we'll also need to register the frame that was select
+    return new_frames
+
 def movingAverage(data, step):
     """ Filters the data noise using Moving Averague algorithm """
     step = int(step)
